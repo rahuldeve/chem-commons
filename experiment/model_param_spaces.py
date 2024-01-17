06cbc6a48c97b8aafe2b56425a2adfa09ab41e38
Rahul Dev E <rahul.e.dev@gmail.com>
2024-01-17 11:31:06 -0500

    update model param spaces

--- experiment/model_param_spaces.py
+++ experiment/model_param_spaces.py
@@ -32,19 +32,20 @@ ParamSpace: TypeAlias = dict[str, ParamType]
 
 lgbm_classifier_param_space: ParamSpace = {
     "lgbmclassifier__objective": ConstantParam("binary"),
-    "lgbmclassifier__metric": ConstantParam("metric"),
+    "lgbmclassifier__metric": ConstantParam("average_precision"),
     "lgbmclassifier__verbosity": ConstantParam(-1),
-    "lgbmclassifier__boosting_type": ConstantParam("gbdt"),
+    "lgbmclassifier__boosting_type": ChoiceParam(["gbdt", "dart"]),
     "lgbmclassifier__reg_alpha": FloatParam(1e-8, 1e-1, log=True),
     "lgbmclassifier__reg_lambda": FloatParam(1e-8, 1e-1, log=True),
     "lgbmclassifier__num_leaves": IntParam(2, 256),
     "lgbmclassifier__subsample": FloatParam(0.1, 1),
     "lgbmclassifier__colsample_bytree": FloatParam(0.1, 1),
     "lgbmclassifier__min_child_samples": IntParam(5, 100),
-    "lgbmclassifier__n_jobs": ConstantParam(4),
+    "lgbmclassifier__n_jobs": ConstantParam(16),
     "lgbmclassifier__random_state": ConstantParam(42),
-    "lgbmclassifier__scale_pos_weight": IntParam(10, 50, 1),
+    "lgbmclassifier__scale_pos_weight": IntParam(30, 100, 1),
     "lgbmclassifier__n_estimators": ConstantParam(2000),
+    "lgbmclassifier__max_depth": IntParam(5, 50)
     # "lgbmclassifier__early_stopping_rounds": ConstantParam(100)
 }
 
